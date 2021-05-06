4cbe8898da64b6419112762f4d03e9a5da94857f
monicachandra <56423441+monicachandra@users.noreply.github.com>
2021-05-06 14:42:58 +0700

    Add files via upload

--- utils.js
+++ utils.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    database: "soainf20202_m8",
-    user: "root",
-    password: ""
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
 });
 
 function getConnection() {
